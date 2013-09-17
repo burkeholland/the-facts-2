de2d7dd58a94aec78744faaa514d41024355cac0
Burke Holland <burkeholland@gmail.com>
2013-09-17 16:30:31 -0500

    Changes made by Burke Holland (burkeholland@gmail.com).

--- the-facts/scripts/app.js
+++ the-facts/scripts/app.js
@@ -1,6 +1,19 @@
+// global error handling
+var showAlert = function(message, title, callback) {
+    navigator.notification.alert(message, callback || function () {
+    }, title, 'OK');
+};
+
+window.addEventListener('error', function (e) {
+    e.preventDefault();
+    var message = e.message + "' from " + e.filename + ":" + e.lineno;
+    showAlert(message, 'Error occured');
+    return true;
+});
+
 (function (global) {
     var application,
-        api = "http://localhost:52787/";
+        api = "http://chuckfacts.azurewebsites.net/";
     
     global.app = {};
     
@@ -81,8 +94,20 @@
     	})
     });
     
+    global.app.share = function() {
+        window.plugins.social.available(function(avail) {
+            if (avail) {
+                alert("SWEET!");
+            } else {
+                alert("DAMMIT");
+            }
+        });
+       	window.plugins.social.share('This is the message you want to share', null, null);
+    };
+    
     document.addEventListener("deviceready", function () {
         application = new kendo.mobile.Application(document.body, { transition: "slide", skin: "flat" });
+        
     }, false);
     
     
